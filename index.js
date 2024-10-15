5c394d0b8d8cbb2d2635e2e88499340d12eaaa62
Canales Santos <ecanales@mtc.gob.pe>
2024-10-15 14:25:52 -0500

    Subida3

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const admin = require("firebase-admin");
+const dotenv = require("dotenv");
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG);
+
+admin.initializeApp({
+  credential: cert(firebaseConfig),
+});
+
+const db = getFirestore();
+
+app.get("/", (req, res) => {
+  res.send("¡Bienvenido al servidor express!");
+});
+
+// Endpoint básico (GET)
+app.get("/data", async (req, res) => {
+  try {
+    const snapshot = await db.collection("Departamentos").get();
+    const departamentos = [];
+    snapshot.forEach((doc) => {
+      departamentos.push({ id: doc.id, ...doc.data() });
+    });
+    res.status(200).json(departamentos);
+  } catch (error) {
+    console.log("Error obteniendo datos: ", error);
+    res.status(500).send("Error al obtener los datos");
+  }
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
