64d2a773a39c979e4ca6aa99b3a567a0dfad4d4a
Canales Santos <ecanales@mtc.gob.pe>
2024-10-16 15:31:42 -0500

    Subida12

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.get("/", (req, res) => {
   res.send("¡Bienvenido al servidor express!");
 });
 
-app.get("/data", async (req, res) => {
+//Obtener Deparrtamentos
+app.get("/dep", async (req, res) => {
   try {
     const snapshot = await db.collection("Departamentos").get();
     const departamentos = [];
@@ -33,6 +34,84 @@ app.get("/data", async (req, res) => {
       departamentos.push({ id: doc.id, ...doc.data() });
     });
     res.status(200).json(departamentos);
+  } catch (error) {
+    console.log("Error obteniendo datos: ", error);
+    res.status(500).send("Error al obtener los datos");
+  }
+});
+
+//Obtener Provincias
+app.get("/prov", async (req, res) => {
+  const { idDep } = req.query;
+  if (!idDep) {
+    return res.status(400).send("Falta el parámetro departamento");
+  }
+
+  try {
+    const snapshot = await db
+      .collection("Departamentos")
+      .doc(idDep)
+      .collection("Provincias")
+      .get();
+
+    console.log(snapshot);
+
+    const provincias = [];
+    snapshot.forEach((doc) => {
+      provincias.push({ id: doc.id, ...doc.data() });
+    });
+    res.status(200).json(provincias);
+  } catch (error) {
+    console.log("Error obteniendo datos: ", error);
+    res.status(500).send("Error al obtener los datos");
+  }
+});
+
+// Obtener Distritos
+app.get("/dist", async (req, res) => {
+  const { idDep } = req.query;
+  const { idProv } = req.query;
+  if (!idDep || !idProv) {
+    return res.status(400).send("Faltan los parámetros idDep o idProv");
+  }
+  try {
+    const snapshot = await db
+      .collection("Departamentos")
+      .doc(idDep)
+      .collection("Provincias")
+      .doc(idProv)
+      .collection("Distritos")
+      .get();
+
+    console.log(snapshot);
+
+    const distritos = [];
+    snapshot.forEach((doc) => {
+      distritos.push({ id: doc.id, ...doc.data() });
+    });
+    res.status(200).json(distritos);
+  } catch (error) {
+    console.log("Error obteniendo datos: ", error);
+    res.status(500).send("Error al obtener los datos");
+  }
+});
+
+//Obtener Ecsales
+app.get("/ecsales", async (req, res) => {
+  const { ubicacion } = req.query;
+  if (!ubicacion) {
+    return res.status(400).send("Falta el parámetro ubicacion");
+  }
+  try {
+    const snapshot = await db
+      .collection("Ecsales")
+      .where("distrito", "==", distrito)
+      .get();
+    const ecsales = [];
+    snapshot.forEach((doc) => {
+      ecsales.push({ id: doc.id, ...doc.data() });
+    });
+    res.status(200).json(ecsales);
   } catch (error) {
     console.log("Error obteniendo datos: ", error);
     res.status(500).send("Error al obtener los datos");
