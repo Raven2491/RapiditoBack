ce8116d0e5b1a258b6511ac47397c5f368c49a55
Canales Santos <ecanales@mtc.gob.pe>
2024-10-22 10:35:43 -0500

    Subida13

--- index.js
+++ index.js
@@ -98,20 +98,108 @@ app.get("/dist", async (req, res) => {
 
 //Obtener Ecsales
 app.get("/ecsales", async (req, res) => {
-  const { ubicacion } = req.query;
-  if (!ubicacion) {
-    return res.status(400).send("Falta el parámetro ubicacion");
+  const { dep, prov, dist } = req.query;
+
+  if (!dep) {
+    return res
+      .status(400)
+      .send("Seleccione al menos un departamento para filtrar");
   }
+
   try {
-    const snapshot = await db
-      .collection("Ecsales")
-      .where("distrito", "==", distrito)
-      .get();
+    let query = db.collection("Ecsales").where("departamento", "==", dep);
+
+    if (prov) {
+      query = query.where("provincia", "==", prov);
+    }
+
+    if (dist) {
+      query = query.where("distrito", "==", dist);
+    }
+
+    const snapshot = await query.get();
+
     const ecsales = [];
     snapshot.forEach((doc) => {
       ecsales.push({ id: doc.id, ...doc.data() });
     });
+
     res.status(200).json(ecsales);
+  } catch (error) {
+    console.error("Error obteniendo datos: ", error);
+    res.status(500).send("Error al obtener los datos");
+  }
+});
+
+//Ubicar Ecsales
+app.get("/ubicar", async (req, res) => {
+  const { nomEcsal } = req.query;
+
+  if (!nomEcsal) {
+    return res
+      .status(400)
+      .send("Seleccione al menos un nombre de Ecsales para filtrar");
+  }
+
+  try {
+    let query = db.collection("Ecsales").where("razonsocial", "==", nomEcsal);
+
+    const snapshot = await query.get();
+
+    const ecsal = [];
+    snapshot.forEach((doc) => {
+      ecsal.push({ id: doc.id, ...doc.data() });
+    });
+
+    res.status(200).json(ecsal);
+  } catch (error) {
+    console.error("Error obteniendo datos: ", error);
+    res.status(500).send("Error al obtener los datos");
+  }
+});
+
+//InfoEcsal
+app.get("/infoecsal", async (req, res) => {
+  const { nomEcsal } = req.query;
+
+  if (!nomEcsal) {
+    return res
+      .status(400)
+      .send("Seleccione al menos un nombre de Ecsales para filtrar");
+  }
+
+  try {
+    let query = db.collection("Ecsales").where("razonsocial", "==", nomEcsal);
+
+    const snapshot = await query.get();
+
+    if (snapshot.empty) {
+      return res.status(404).send("No se encontraron Ecsales con ese nombre");
+    }
+
+    let ecsal = [];
+
+    snapshot.forEach((doc) => {
+      ecsal.push({ id: doc.id, ...doc.data() });
+    });
+
+    res.status(200).json(ecsal);
+  } catch (error) {
+    console.error("Error obteniendo datos: ", error);
+    res.status(500).send("Error al obtener los datos");
+  }
+});
+
+//Obtener Entidades todas
+
+app.get("/entidades", async (req, res) => {
+  try {
+    const snapshot = await db.collection("Entidades").get();
+    const entidades = [];
+    snapshot.forEach((doc) => {
+      entidades.push({ id: doc.id, ...doc.data() });
+    });
+    res.status(200).json(entidades);
   } catch (error) {
     console.log("Error obteniendo datos: ", error);
     res.status(500).send("Error al obtener los datos");
